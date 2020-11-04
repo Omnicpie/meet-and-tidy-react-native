53729513ec4300e9ddb06854adc198f07d4cf481
Beth Hubbard <beth.hubbard@commit.digital>
2020-11-04 12:04:55 +0000

    Update app

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View, Image, ScrollView} from 'react-native';
+import { Button, StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
+// import (useDimensions) from '@react-native-community/hooks';
+
 import Bullets from './Bullets.js'
-import PopularEvents from './PopularEvents.js'
+import PopularEventScroll from './PopularEventScroll.js'
 import Create from './Create.js'
 import Logo from './Logo.js'
-import SearchBar from './SearchBar.js'
+import { useDimensions } from '@react-native-community/hooks';
+// import SearchBar from './SearchBar.js'
+// import BottomNavBar from './BottomNavBar.js'
 
 export default function App() {
   return (
-    <View>
+    <SafeAreaView>
       <Create />
       <Logo />
-      <SearchBar />
-      <PopularEvents />
-      <Bullets/>
-    </View>
+      <View>
+      {/* <SearchBar /> */}
+        <PopularEventScroll />
+        <Bullets/>
+      </View>
+      {/* <BottomNavBar /> */}
+    </SafeAreaView>
   )
 }
 
