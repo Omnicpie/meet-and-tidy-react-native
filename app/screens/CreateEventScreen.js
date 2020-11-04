5d5c7d2ad9c54953c56c8106f132c12e79d2dea5
Beth Hubbard <beth.hubbard@commit.digital>
2020-11-04 16:48:39 +0000

    Update create event screen

--- app/screens/CreateEventScreen.js
+++ app/screens/CreateEventScreen.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import Bullets from '../assets/Bullets'
-
 import { StyleSheet, Button, Text, View, Image, SafeAreaView, TouchableOpacity } from 'react-native';
 
 
 function CreateEventScreen(props) {
     return (
-        <SafeAreaView>
- 
-        <View>
-            CREATE EVENT SCREEN
-          <Button
-        title="Go to CreateEvent"
-        onPress={() =>
-          navigation.navigate('CreateEvent', { name: 'CreateEvent' })
-        }
-      />
-        </View>
-   
+      <SafeAreaView style={styles.container}>
+        <Text>A 'Create an event' form will be here </Text>
       </SafeAreaView> 
     );
 }
 
 export default CreateEventScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 25
+  }
+});
