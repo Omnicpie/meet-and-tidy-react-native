6c2bbfcdf5c2b3299b4c093f471191ce94d90a40
Beth Hubbard <beth.hubbard@commit.digital>
2020-11-18 11:19:28 +0000

    Add to eventscreen

--- app/screens/EventScreen.js
+++ app/screens/EventScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
-  StyleSheet, ActivityIndicator, SafeAreaView, Text, View,
+  StyleSheet, ActivityIndicator, SafeAreaView, Text, View
 } from 'react-native';
 import Request from '../helpers/Request';
 
@@ -24,7 +24,26 @@ function EventScreen(props) {
       {isLoading ? <ActivityIndicator /> : (
         <View>
           <Text style={styles.primaryHeading}>{data.title}</Text>
-          <Text style={styles.paragraph}>{data.description}</Text>
+          <View>
+            <Text style={styles.tile}>Image</Text>
+          </View>
+          <View>
+            <Text style={styles.subheading}>Description:</Text>
+            <Text style={styles.paragraph}>{data.description}</Text>
+          </View>
+          <View>
+            <Text style={styles.subheading}>Date of event:</Text>
+            <Text style={styles.paragraph}>{data.date}</Text>
+          </View>
+          <View>
+            <Text style={styles.subheading}>Type of event:</Text>
+            <Text style={styles.paragraph}>{data.eventType}</Text>
+          </View>
+          <View>
+            <Text style={styles.subheading}>Facilities available:</Text>
+            <Text style={styles.paragraph}>{data.facilityType}</Text>
+          </View>
+          <BottomNavBar navigation={navigation} />
         </View>
       )}
     </SafeAreaView>
@@ -37,14 +56,24 @@ const styles = StyleSheet.create({
   eventContainer: {
     flex: 1,
     backgroundColor: '#fff',
-
   },
   primaryHeading: {
     textAlign: 'center',
-    fontSize: 20,
+    fontSize: 23,
   },
   paragraph: {
     textAlign: 'center',
     width: '100%',
+  },
+  subheading: {
+    textAlign: 'center',
+    width: '100%',
+    fontSize: 15,
+    fontWeight: "bold"
+  },
+  tile: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'rgb(84, 174, 51)',
   },
 });
