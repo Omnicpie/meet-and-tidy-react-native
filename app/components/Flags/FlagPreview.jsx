81f9f28f4df67cf56599d0b7817b7f479d89aec1
Beth Hubbard <beth.hubbard@commit.digital>
2020-11-19 12:39:21 +0000

    Update flag preview

--- app/components/Flags/FlagPreview.jsx
+++ app/components/Flags/FlagPreview.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  StyleSheet, Text, SafeAreaView,
+  StyleSheet, Text, SafeAreaView, View
 } from 'react-native';
 import { ProgressBar, Colors } from 'react-native-paper';
 
@@ -10,42 +10,33 @@ export default function FlagPreview({
   location, title, description, flagType, image, date,
 } = props) {
   return (
-    <SafeAreaView>
-      <Text>Preview flag</Text>
-      <Text>
-        Title:
-        {title}
-      </Text>
-      <Text>
-        Date:
-        {date}
-      </Text>
-      <Text>
-        Description:
-        {description}
-      </Text>
-      <Text>
-        Type:
-        {flagType}
-      </Text>
-      <Text>
-        Location:
-        {location}
-      </Text>
-      <Text>
-        Image:
-        {image}
-      </Text>
-
+    <SafeAreaView style={styles.flagContainer}>
+      <View>
+        <Text style={styles.subheading}>Preview flag</Text>
+        <Image>{image}</Image>
+        <Text style={styles.primaryHeading}>{title}</Text>
+        <Text style={styles.secondaryHeading}>{flagType}</Text>
+        <View>
+          <Text style={styles.subheading}>Date:</Text>
+          <Text style={styles.paragraph}>{date}</Text>
+        </View>
+        <View>
+            <Text style={styles.subheading}>Description:</Text>
+            <Text style={styles.paragraph}>{description}</Text>
+        </View>
+        <View>
+            <Text style={styles.subheading}>Location:</Text>
+            <Text style={styles.infoTile}>{location}</Text>
+        </View>
+      </View>
       <ProgressBar style={styles.progressBar} progress={1} color={Colors.green500} />
       <Text>5 of 5</Text>
     </SafeAreaView>
-
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  flagContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
@@ -54,5 +45,45 @@ const styles = StyleSheet.create({
   },
   progressBar: {
     marginTop: 50,
+  },
+  paragraph: {
+    textAlign: 'left',
+    width: '100%',
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  primaryHeading: {
+    textAlign: 'center',
+    fontSize: 23,
+    paddingTop: 10,
+    paddingBottom: 3
+  },
+  secondaryHeading: {
+    textAlign: 'center',
+    width: '100%',
+    paddingLeft: 15,
+  },
+  subheading: {
+    textAlign: 'left',
+    width: '100%',
+    fontSize: 15,
+    fontWeight: "bold",
+    paddingTop: 10,
+    paddingLeft: 15,
+    marginBottom: 5,
+  },
+  infoTile: {
+    width: '85',
+    height: 150,
+    backgroundColor: 'rgb(84, 174, 51)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  imageSelected: {
+    width: '100%',
+    height: 200,
   },
 });
