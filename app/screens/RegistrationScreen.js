1996a95d2438a56be0df8f8c8b35db842da5a8ad
Beth Hubbard <beth.hubbard@commit.digital>
2020-11-19 09:35:14 +0000

    Edit registration screen

--- app/screens/RegistrationScreen.js
+++ app/screens/RegistrationScreen.js
@@ -4,7 +4,30 @@ import { StyleSheet, SafeAreaView, Text } from 'react-native';
 function RegistrationScreen(props) {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>A registration form will be here </Text>
+      <Text style={styles.primaryHeading}>Registration</Text>
+      <Text style={styles.subheading}>Explanation of why we need this info and how we use it.</Text>
+      <View>
+        <Text style={styles.subheading}>Name</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+        />
+      </View>
+      <View>
+        <Text style={styles.subheading}>Email</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+        />
+      </View>
+      <View style={styles.buttonStyle}>
+          <Button
+            title="Submit"
+          />
+        </View>
+      <BottomNavBar navigation={navigation} />
     </SafeAreaView>
   );
 }
@@ -19,4 +42,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingVertical: 25,
   },
+  primaryHeading: {
+    textAlign: 'center',
+    fontSize: 23,
+    paddingTop: 10,
+  },
+  subheading: {
+    textAlign: 'center',
+    width: '100%',
+    fontSize: 15,
+    fontWeight: "bold",
+    paddingTop: 20,
+    paddingLeft: 15,
+  },
+  buttonStyle: {
+    display: 'flex',
+    alignContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
+
