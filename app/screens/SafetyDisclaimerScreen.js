78d848407c98d96703fddef4d77d8aef7a1b682d
Beth Hubbard <beth.hubbard@commit.digital>
2020-11-19 09:55:41 +0000

    Edit/style safety disclaimer

--- app/screens/SafetyDisclaimerScreen.js
+++ app/screens/SafetyDisclaimerScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, Button } from 'react-native';
+import BottomNavBar from '../components/BottomNavBar';
 
-function SafetyDisclaimerScreen(props) {
+
+function SafetyDisclaimerScreen(navigation) {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>A safety disclaimer will be here </Text>
+      <View>
+        <Text style={styles.primaryHeading}>Safety Disclaimer</Text>
+        <Text style={styles.paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Text>
+      </View>
+      <BottomNavBar navigation={navigation} />
     </SafeAreaView>
   );
 }
@@ -18,5 +24,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 25,
+  },
+  primaryHeading: {
+    textAlign: 'Left',
+    fontSize: 23,
+    paddingTop: 10,
+  },
+  paragraph: {
+    textAlign: 'left',
+    width: '100%',
+    paddingLeft: 15,
+    paddingRight: 15,
   },
 });
