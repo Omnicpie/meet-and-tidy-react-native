7087baf6dcd66d9ec653f6ec35d1e82a233d4bd2
Beth Hubbard <beth.hubbard@commit.digital>
2020-11-20 10:28:25 +0000

    Separate css for reg screen

--- app/screens/RegistrationScreen.js
+++ app/screens/RegistrationScreen.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, TextInput, Text, View, Button } from 'react-native';
 import BottomNavBar from '../components/BottomNavBar';
-
+import Main from '../assets/stylesheets/Main';
 
 function RegistrationScreen({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.primaryHeading}>Registration</Text>
-      <Text style={styles.subheading}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Text>
+    <SafeAreaView style={Main.container}>
+      <Text style={Main.regHeading}>Registration</Text>
+      <Text style={Main.regSubheading}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Text>
       <View>
-        <Text style={styles.secondaryHeading}>Username</Text>
+        <Text style={Main.regSecondaryHeading}>Username</Text>
         <TextInput
-          style={styles.textInput}
+          style={Main.regTextInput}
         />
       </View>
       <View>
-        <Text style={styles.secondaryHeading}>Email</Text>
+        <Text style={Main.regSecondaryHeading}>Email</Text>
         <TextInput
-          style={styles.textInput}
+          style={Main.textInput}
         />
       </View>
-      <View style={styles.buttonStyle}>
+      <View style={Main.regButtonStyle}>
         <Button
           title="Submit"
           onPress={() => navigation.navigate('SafetyDisclaimer')}
@@ -32,52 +32,3 @@ function RegistrationScreen({ navigation }) {
 }
 
 export default RegistrationScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 25,
-  },
-  textInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    width: '100%',
-  },
-  primaryHeading: {
-    textAlign: 'center',
-    fontSize: 25,
-    marginBottom: 28,
-  },
-  secondaryHeading: {
-    textAlign: 'center',
-    width: '100%',
-    paddingLeft: 15,
-    paddingRight: 15,
-    fontSize: 15,
-    marginBottom: 5,
-    marginTop: 10,
-  },
-  subheading: {
-    textAlign: 'center',
-    marginTop: 5,
-    width: '100%',
-    fontSize: 15,
-    fontWeight: "bold",
-    padding: 10,
-    marginBottom: 25,
-  },
-  buttonStyle: {
-    display: 'flex',
-    alignContent: 'center',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 50,
-    paddingBottom: 75,
-  },
-});
-
