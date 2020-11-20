ff6e4667399f1e2e3a42f02b709f95ff2be43fa6
Beth Hubbard <beth.hubbard@commit.digital>
2020-11-20 12:57:38 +0000

    Add separate css for flag and event creation

---
+++ app/assets/stylesheets/CreateFlag.jsx
@@ -0,0 +1,28 @@
+export default {
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingBottom: 150,
+  },
+  progressBar: {
+    marginTop: 50,
+  },
+  textInput: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 50,
+  },
+  primaryHeading: {
+    textAlign: 'center',
+    fontSize: 25,
+    marginTop: 50,
+    marginBottom: 50,
+  },
+  centeredText: {
+    textAlign: 'center',
+    marginTop: 5,
+  },
+};
