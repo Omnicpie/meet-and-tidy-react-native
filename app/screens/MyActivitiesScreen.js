163390b01674bc5e01f35716ae3b504725ce6e91
Beth Hubbard <beth.hubbard@commit.digital>
2020-12-18 14:03:15 +0000

    Add my activities stylesheet

--- app/screens/MyActivitiesScreen.js
+++ app/screens/MyActivitiesScreen.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { ScrollView, SafeAreaView, View, Text } from 'react-native';
 import BottomNavBar from '../components/BottomNavBar';
-import Main from '../assets/stylesheets/Main';
+import Activities from '../assets/stylesheets/Activities';
 
 function MyActivitiesScreen({ navigation }) {
   return (
-    <SafeAreaView style={Main.mainContainer}>
-      <ScrollView>
-        <View>
-          <Text style={Main.subheading}>Events I'm Attending</Text>
-          <Text style={Main.paragraph}>You're not attending any events</Text>
+    <SafeAreaView style={Activities.Container}>
+      <ScrollView style={Activities.scrollContainer}>
+        <Text style={Activities.primaryHeading}>My Activities</Text>
+        <View style={Activities.even}>
+          <Text style={Activities.subheading}>Events I'm Attending</Text>
+          <Text style={Activities.paragraph}>You're not attending any events</Text>
         </View>
-        <View>
-          <Text style={Main.subheading}>Events I'm Organising</Text>
-          <Text style={Main.paragraph}>You're not organising any events</Text>
+        <View style={Activities.odd}>
+          <Text style={Activities.subheading}>Events I'm Organising</Text>
+          <Text style={Activities.paragraph}>You're not organising any events</Text>
         </View>
-        <View>
-          <Text style={Main.subheading}>Messes I've Flagged</Text>
-          <Text style={Main.paragraph}>1</Text>
+        <View style={Activities.even}>
+          <Text style={Activities.subheading}>Messes I've Flagged</Text>
+          <Text style={Activities.paragraph}>1</Text>
+        </View>
+        <View style={Activities.odd}>
+          <Text style={Activities.subheading}>Your badges</Text>
+          <Text style={Activities.paragraph}>You don't have any badges at the moment</Text>
         </View>
       </ScrollView>
       <BottomNavBar navigation={navigation} />
