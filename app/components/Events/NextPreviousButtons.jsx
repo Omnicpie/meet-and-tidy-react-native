90754ab5ce57bb5cae3e14031d2ec4ce220a8008
Beth Hubbard <beth.hubbard@commit.digital>
2020-12-18 16:29:11 +0000

    Add back button to event preview Co-author: Aman

--- app/components/Events/NextPreviousButtons.jsx
+++ app/components/Events/NextPreviousButtons.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import {
   StyleSheet, View, Button,
 } from 'react-native';
+import Events from '../../assets/stylesheets/Events';
 
 export default function NextPreviousButtons({
   onNext, onPrevious
 } = props) {
   return (
     <View>
-      <View style={styles.buttonContianer}>
-        <View style={styles.buttonElement}>
+      <View style={Events.buttonContianer}>
+        <View style={Events.buttonElement}>
           <Button
             onPress={onPrevious}
             title="Previous"
           />
         </View>
-        <View style={styles.buttonElement}>
+        <View style={Events.buttonElement}>
           <Button
             onPress={onNext}
             title="Next"
@@ -25,40 +26,3 @@ export default function NextPreviousButtons({
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  progressBar: {
-    marginTop: 50,
-  },
-  textInput: {
-    height: 40,
-    width: '100%',
-    textAlign: 'center',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 50,
-
-  },
-  buttonContianer: {
-    display: 'flex',
-    alignContent: 'center',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonElement: {
-    width: '48%',
-    marginRight: 5,
-    marginLeft: 5,
-  },
-  primaryHeading: {
-    textAlign: 'center',
-    fontSize: 25,
-    marginTop: 50,
-    marginBottom: 50,
-  },
-  centeredText: {
-    textAlign: 'center',
-    marginTop: 5,
-  },
-});
