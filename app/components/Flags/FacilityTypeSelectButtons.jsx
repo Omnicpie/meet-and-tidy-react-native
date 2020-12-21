b69a96b12f637bc3a0a4b740e216a6ca482304d5
Beth Hubbard <beth.hubbard@commit.digital>
2020-12-21 13:29:55 +0000

    Style facility types on rows

--- app/components/Flags/FacilityTypeSelectButtons.jsx
+++ app/components/Flags/FacilityTypeSelectButtons.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import {
-  SafeAreaView, Button,
-} from 'react-native';
+import { SafeAreaView, Button, View } from 'react-native';
+import Flags from '../../assets/stylesheets/Flags';
 
 export default function FacilityTypeSelectButtons({
   onChangeFacilityType,
@@ -9,42 +8,48 @@ export default function FacilityTypeSelectButtons({
   return (
   // TODO this currently only selects the last one from the list
 
-    <SafeAreaView>
-      <Button
-        onPress={onChangeFacilityType('Disabled Access')}
-        facilityType="Disabled Access"
-        title="Disabled Access"
-      />
-      <Button
-        onPress={onChangeFacilityType('Child Friendly')}
-        facilityType="Child Friendly"
-        title="Child Friendly"
-      />
-      <Button
-        onPress={onChangeFacilityType('Toilets Nearby')}
-        facilityType="Toilets Nearby"
-        title="Toilets Nearby"
-      />
-      <Button
-        onPress={onChangeFacilityType('Parking Nearby')}
-        facilityType="Parking Nearby"
-        title="Parking Nearby"
-      />
-      <Button
-        onPress={onChangeFacilityType('First Aiders')}
-        facilityType="First Aiders"
-        title="First Aiders"
-      />
-      <Button
-        onPress={onChangeFacilityType('Transport Routes Nearby')}
-        facilityType="Transport Routes Nearby"
-        title="Transport Routes Nearby"
-      />
-      <Button
-        onPress={onChangeFacilityType('Other')}
-        facilityType="Other"
-        title="Other"
-      />
+    <SafeAreaView style={Flags.flagTypeContainer}>
+      <View style={Flags.buttonContianer}>
+        <Button
+          onPress={onChangeFacilityType('Disabled Access')}
+          facilityType="Disabled Access"
+          title="Disabled Access"
+        />
+        <Button
+          onPress={onChangeFacilityType('Child Friendly')}
+          facilityType="Child Friendly"
+          title="Child Friendly"
+        />
+        <Button
+          onPress={onChangeFacilityType('Toilets Nearby')}
+          facilityType="Toilets Nearby"
+          title="Toilets Nearby"
+        />
+      </View>
+      <View style={Flags.buttonContianer}>
+        <Button
+          onPress={onChangeFacilityType('Parking Nearby')}
+          facilityType="Parking Nearby"
+          title="Parking Nearby"
+        />
+        <Button
+          onPress={onChangeFacilityType('First Aiders')}
+          facilityType="First Aiders"
+          title="First Aiders"
+        />
+      </View>
+      <View style={Flags.buttonContianer}>
+        <Button
+          onPress={onChangeFacilityType('Transport Routes Nearby')}
+          facilityType="Transport Routes Nearby"
+          title="Transport Routes Nearby"
+        />
+        <Button
+          onPress={onChangeFacilityType('Other')}
+          facilityType="Other"
+          title="Other"
+        />
+      </View>
     </SafeAreaView>
   );
 }
