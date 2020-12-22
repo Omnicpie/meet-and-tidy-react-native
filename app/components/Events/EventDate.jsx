68cbec3d81824a8ee704dc5152e4257c356ef6fa
Beth Hubbard <beth.hubbard@commit.digital>
2020-12-22 11:42:07 +0000

    Start date picker

--- app/components/Events/EventDate.jsx
+++ app/components/Events/EventDate.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import {
-  Text, View, TextInput, Button,
-} from 'react-native';
+import React, { useState } from "react";
+import { TextInput, Text, View, Button } from 'react-native';
 import { ProgressBar, Colors } from 'react-native-paper';
-import NextPreviousButtons from './NextPreviousButtons';
 import Events from '../../assets/stylesheets/Events';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 export default function EventDate({
   date, onChangeDate, onNext,
@@ -17,11 +15,36 @@ export default function EventDate({
     }
   }
 
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    hideDatePicker();
+  };
+
   return (
     <View style={Events.mainContainer}>
       <ProgressBar style={Events.progressBar} progress={0.25} color={Colors.green500} />
       <Text style={Events.centeredText} >2 of 8</Text>
       <Text style={Events.primaryHeading} >When is your event?</Text>
+      <View>
+        <Button title="Show Date Picker" onPress={showDatePicker} />
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="datetime"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+          locale="en_GB"
+        />
+      </View>
       <TextInput
         defaultValue={date}
         style={Events.textInput}
