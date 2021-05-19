16c0ae39a64c55d0ae2da8c6699198baec63235c
Beth Hubbard <bah1996@hotmail.co.uk>
2021-05-19 15:31:56 +0100

    Redirect/style registration and safety disclaimer Co-authored-by: georgina<georginavweston@users.noreply.github.com>

--- app/screens/RegistrationScreen.tsx
+++ app/screens/RegistrationScreen.tsx
@@ -1,39 +1,46 @@
-import { ApolloError, gql, useMutation } from '@apollo/client';
-import React, { ReactElement, useState } from 'react';
+import { ApolloError, gql, useMutation } from "@apollo/client";
+import React, { ReactElement, useState } from "react";
 import {
-  Alert, Button, SafeAreaView, ScrollView, TextInput,
-} from 'react-native';
-import { Text, View } from '../components/Themed';
-import BottomNavBar from '../components/BottomNavBar';
-import Main from '../assets/stylesheets/Main';
+  Alert,
+  Button,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  TextInput,
+} from "react-native";
+import { Text, View } from "../components/Themed";
+import BottomNavBar from "../components/BottomNavBar";
+import Main from "../assets/stylesheets/Main";
 
 const CREATE_ACCOUNT = gql`
-mutation CreateAccount($name: String, $email: String!, $password: String!) {
-  createAccount(name: $name, email: $email, password: $password) {
-    errors
+  mutation CreateAccount($name: String, $email: String!, $password: String!) {
+    createAccount(name: $name, email: $email, password: $password) {
+      errors
+    }
   }
-}
 `;
 
 type RegistrationScreenProps = {
   navigation: any;
 };
 
-export default function RegistrationScreen({ navigation }: RegistrationScreenProps): ReactElement {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
+export default function RegistrationScreen({
+  navigation,
+}: RegistrationScreenProps): ReactElement {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
   const responses = {
-    onCompleted() {
-      navigation.navigate('SafetyDisclaimer');
-    },
     onError(error: ApolloError) {
       console.log(error);
     },
   };
 
-  const [createAccount, createAccountResult] = useMutation(CREATE_ACCOUNT, responses);
+  const [createAccount, createAccountResult] = useMutation(
+    CREATE_ACCOUNT,
+    responses
+  );
 
   function formValid(): boolean {
     return email.length >= 1 && name.length >= 1 && password.length >= 6;
@@ -41,8 +48,9 @@ export default function RegistrationScreen({ navigation }: RegistrationScreenPro
 
   let initialDisclaimerValues = {
     title: "Safety Disclaimer",
-    message: "By agreeing to create an account, you confrim that you have read our safety disclaimer. If you have not done so, press cancel and click the link below before progressing forward.",
-  }
+    message:
+      "By agreeing to create an account, you confrim that you have read our safety disclaimer. If you have not done so, press cancel and click the link below the form before progressing forward.",
+  };
 
   const confirmDisclaimer = () => {
     Alert.alert(
@@ -52,45 +60,36 @@ export default function RegistrationScreen({ navigation }: RegistrationScreenPro
         {
           text: "Cancel",
           onPress: () => {
-            return; // else change props in window with a message to say "Please read full conditions to continue."
-          }
+            return;
+          },
         },
         {
           text: "I agree",
           onPress: () => {
-
-          navigation.navigate('Event') && createAccount({ variables: { name, email, password } })
-          }
+            navigation.navigate("Event");
+            createAccount({ variables: { name, email, password } });
+          },
         },
-           /// trigger affialte window to trigger disclaimer (with a link to full details on homepage)}
-
       ],
       { cancelable: false }
     );
-  }
-
-//{id: item.product.id, title: item.product.name,}
-
-
-
-  const onSubmit = () => {
-    // if ok set agree to true in db then reeirect to a successful page (events?) and create user
-    if (formValid()) {
-      confirmDisclaimer();
-    // set agree to true in db then reeirect to a successful page(events ?) and create user
-
-    }
   };
 
+  const onSubmit = () => {
+    if (formValid()) {
+      confirmDisclaimer();
+    }
+  };
 
   return (
     <SafeAreaView style={Main.mainContainer}>
       <ScrollView>
         <Text style={Main.regHeading}>Hello there!</Text>
         <Text style={Main.regSubheading}>
-          Introduce yourself so the organiser and other attendees can see who is attending.
-          Your email address is used to let you know about any changes to this event.
-          We won’t share your details or send you any marketing without your permission.
+          Introduce yourself so the organiser and other attendees can see who is
+          attending. Your email address is used to let you know about any
+          changes to this event. We won’t share your details or send you any
+          marketing without your permission.
         </Text>
         <View>
           <Text style={Main.regSecondaryHeading}>Your name</Text>
@@ -123,12 +122,15 @@ export default function RegistrationScreen({ navigation }: RegistrationScreenPro
             value={password}
           />
         </View>
+        <View>
+          <Pressable onPress={() => navigation.navigate("SafetyDisclaimer")}>
+            <Text style={Main.safetyDisText}>
+              Please read Safety Disclaimer {"\n"}before continuing
+            </Text>
+          </Pressable>
+        </View>
         <View style={Main.regButtonStyle}>
-          <Button
-            disabled={!formValid()}
-            title="Submit"
-            onPress={onSubmit}
-          />
+          <Button disabled={!formValid()} title="Submit" onPress={onSubmit} />
         </View>
       </ScrollView>
       <BottomNavBar navigation={navigation} />
