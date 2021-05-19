237cf6f242166df27d96c68cf243085dafe2f5e7
Beth Hubbard <bah1996@hotmail.co.uk>
2021-05-19 14:23:10 +0100

    Link up registration/disclaimer process wip

--- app/screens/RegistrationScreen.tsx
+++ app/screens/RegistrationScreen.tsx
@@ -1,15 +1,11 @@
 import { ApolloError, gql, useMutation } from '@apollo/client';
 import React, { ReactElement, useState } from 'react';
 import {
-  Button, SafeAreaView, ScrollView, TextInput,
+  Alert, Button, SafeAreaView, ScrollView, TextInput,
 } from 'react-native';
 import { Text, View } from '../components/Themed';
 import BottomNavBar from '../components/BottomNavBar';
 import Main from '../assets/stylesheets/Main';
-
-type RegistrationScreenProps = {
-  navigation: any;
-};
 
 const CREATE_ACCOUNT = gql`
 mutation CreateAccount($name: String, $email: String!, $password: String!) {
@@ -18,6 +14,10 @@ mutation CreateAccount($name: String, $email: String!, $password: String!) {
   }
 }
 `;
+
+type RegistrationScreenProps = {
+  navigation: any;
+};
 
 export default function RegistrationScreen({ navigation }: RegistrationScreenProps): ReactElement {
   const [email, setEmail] = useState('');
@@ -39,13 +39,49 @@ export default function RegistrationScreen({ navigation }: RegistrationScreenPro
     return email.length >= 1 && name.length >= 1 && password.length >= 6;
   }
 
+  let initialDisclaimerValues = {
+    title: "Safety Disclaimer",
+    message: "By agreeing to create an account, you confrim that you have read our safety disclaimer. If you have not done so, press cancel and click the link below before progressing forward.",
+  }
+
+  const confirmDisclaimer = () => {
+    Alert.alert(
+      initialDisclaimerValues.title,
+      initialDisclaimerValues.message,
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            return; // else change props in window with a message to say "Please read full conditions to continue."
+          }
+        },
+        {
+          text: "I agree",
+          onPress: () => {
+
+          navigation.navigate('Event') && createAccount({ variables: { name, email, password } })
+          }
+        },
+           /// trigger affialte window to trigger disclaimer (with a link to full details on homepage)}
+
+      ],
+      { cancelable: false }
+    );
+  }
+
+//{id: item.product.id, title: item.product.name,}
+
+
+
   const onSubmit = () => {
+    // if ok set agree to true in db then reeirect to a successful page (events?) and create user
     if (formValid()) {
-      createAccount({
-        variables: { name, email, password },
-      });
+      confirmDisclaimer();
+    // set agree to true in db then reeirect to a successful page(events ?) and create user
+
     }
   };
+
 
   return (
     <SafeAreaView style={Main.mainContainer}>
