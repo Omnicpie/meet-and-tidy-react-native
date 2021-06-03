e5eb5207bfb1f81e9c50a980a86664b2ee59dc71
Beth Hubbard <beth.hubbard@commit.digital>
2021-06-03 16:24:01 +0100

    Add types file

---
+++ types.d.ts
@@ -0,0 +1,24 @@
+export type RailsId = string | number;
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  CreateMess: undefined;
+  MyActivities: undefined;
+};
+
+export type HomeParamList = {
+  HomeScreen: undefined;
+};
+
+export type CreateMessParamList = {
+  CreateMessScreen: undefined;
+};
+
+export type MyActivitiesParamList = {
+  MyActivitiesScreen: undefined;
+};
