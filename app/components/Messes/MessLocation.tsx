cdd1e7db3dea940ddd0bb293e64c5c4b6736efd9
Beth Hubbard <bah1996@hotmail.co.uk>
2021-06-16 14:27:43 +0100

    Update mess location

--- app/components/Messes/MessLocation.tsx
+++ app/components/Messes/MessLocation.tsx
@@ -1,16 +1,28 @@
 import React, { ReactElement } from 'react';
-import { SafeAreaView, ScrollView, Text, TextInput, View } from 'react-native';
+import {
+  Button, Text, View, TextInput, SafeAreaView,
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { ProgressBar, Colors } from 'react-native-paper';
-import PossibleMapView from '../PossibleMapView';
 import Events from '../../assets/stylesheets/Events';
 import Main from '../../assets/stylesheets/Main';
+import PossibleMapView from '../PossibleMapView';
 
 type MessLocationProps = {
   location: string;
   onChangeLocation: (location: string) => void;
+  onNext: () => void;
 };
 
-export default function MessLocation({ location, onChangeLocation }: MessLocationProps): ReactElement {
+export default function MessLocation({ location, onChangeLocation, onNext }:
+  MessLocationProps): ReactElement {
+  function validateInput() {
+    if (location.length > 2) {
+      onNext();
+    } else {
+      alert('Please enter a location name that is at least 3 characters long.');
+    }
+  }
 
   const region = {
     latitude: 54.3781,
@@ -26,7 +38,7 @@ export default function MessLocation({ location, onChangeLocation }: MessLocatio
         <Text style={Events.centeredText}>1 of 6</Text>
         <View>
           <Text style={Events.primaryHeading}>Where is your mess?</Text>
-          <Text style={Main.tellUsMore}>Tell us where you found this mess</Text>
+          <Text style={Main.tellUsMore}>Tell us where you found this mess.</Text>
         </View>
         <TextInput
           defaultValue={location}
@@ -38,6 +50,12 @@ export default function MessLocation({ location, onChangeLocation }: MessLocatio
         />
         <View style={Events.container}>
           <PossibleMapView region={region}></PossibleMapView>
+        </View>
+        <View style={Events.buttonContainer} marginBottom={75}>
+          <Button
+            onPress={validateInput}
+            title="Next"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
