dc93f48d65403525507fc128fbed6bc6134612e4
Beth Hubbard <beth.hubbard@commit.digital>
2021-07-01 16:52:22 +0100

    Add store ts

---
+++ app/redux/Store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userSliceReducer from './slices/UserSlice';
+
+const store = configureStore({
+  reducer: userSliceReducer,
+});
+
+export default store;
