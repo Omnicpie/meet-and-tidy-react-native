72303a5d81c0e7bb961cb30a0defdb98c97f5fc9
Beth Hubbard <beth.hubbard@commit.digital>
2021-07-01 16:51:25 +0100

    Add user slice

---
+++ app/redux/slices/UserSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ApiUser } from '../../../ApiTypes';
+
+interface UserState {
+  value: ApiUser | null
+}
+
+const initialState = { value: null } as UserState;
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser(state, action: PayloadAction<ApiUser>) {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
