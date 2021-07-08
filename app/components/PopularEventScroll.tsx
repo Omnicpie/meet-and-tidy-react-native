48f39ef015fc5f9d92e0bd926f70953b05562652
Beth Hubbard <bah1996@hotmail.co.uk>
2021-07-08 15:54:54 +0100

    Edit pop scroll wip

--- app/components/PopularEventScroll.tsx
+++ app/components/PopularEventScroll.tsx
@@ -1,14 +1,18 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement } from "react";
 import {
-  ActivityIndicator, FlatList, Pressable, Text, View,
-} from 'react-native';
-import { gql, useQuery } from '@apollo/client';
-import ApiImage from '../helpers/ApiImage';
-import { dayOfMonth, shortMonthName } from '../helpers/DateHelpers';
-import Main from '../assets/stylesheets/Main';
+  ActivityIndicator,
+  FlatList,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import ApiImage from "../helpers/ApiImage";
+import { dayOfMonth, shortMonthName } from "../helpers/DateHelpers";
+import Main from "../assets/stylesheets/Main";
 
-import { ApiEvent } from '../../ApiTypes';
-import ErrorPanel from './ErrorPanel';
+import { ApiEvent } from "../../ApiTypes";
+import ErrorPanel from "./ErrorPanel";
 
 type PopularEventScrollProps = {
   navigation: any;
@@ -22,18 +26,32 @@ const POPULAR_EVENTS_QUERY = gql`
       description
       startsAt
       imageUrls
+      attendances {
+        eventId
+        userId
+      }
     }
   }
 `;
 
-export default function PopularEventScroll({ navigation }: PopularEventScrollProps): ReactElement {
-  const {
-    data, error, loading, refetch,
-  } = useQuery(POPULAR_EVENTS_QUERY);
+export default function PopularEventScroll({
+  navigation,
+}: PopularEventScrollProps): ReactElement {
+  const { data, error, loading, refetch } = useQuery(POPULAR_EVENTS_QUERY);
 
   if (loading) {
     return <ActivityIndicator />;
   }
+
+  // Add attendences to query on this page and pull in events
+  console.log(data.events);
+
+  // Isolate the attendances array and order items based on which has the highest amount of userId's each
+  data.events.map((event) => (
+     console.log(event)
+  ))}
+
+  // Str
 
   if (error) {
     return <ErrorPanel message={error.message} reload={refetch} />;
@@ -55,18 +73,20 @@ export default function PopularEventScroll({ navigation }: PopularEventScrollPro
           horizontal
           keyExtractor={({ id }) => id.toString()}
           renderItem={({ item }) => (
-            <Pressable onPress={() => navigation.navigate('Event', { id: item.id })}>
+            <Pressable
+              onPress={() => navigation.navigate("Event", { id: item.id })}
+            >
               <View style={Main.scrollerTile}>
                 {firstImage(item)}
                 <View style={Main.scrollerTileLower}>
                   <View style={Main.scrollerTileLeft}>
                     <Text style={Main.date}>{dayOfMonth(item.startsAt)}</Text>
-                    <Text style={Main.month}>{shortMonthName(item.startsAt)}</Text>
+                    <Text style={Main.month}>
+                      {shortMonthName(item.startsAt)}
+                    </Text>
                   </View>
                   <View style={Main.scrollerTileRight}>
-                    <Text style={Main.eventTitle}>
-                      {item.title}
-                    </Text>
+                    <Text style={Main.eventTitle}>{item.title}</Text>
                     <Text style={Main.eventDescription} numberOfLines={1}>
                       {item.description}
                     </Text>
